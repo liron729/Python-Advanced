e4299be426ab985c7ef3c04eb65053d78c5ff2fe
liron729 <lironaliu8@gmail.com>
2026-02-10 17:48:53 +0100

    lironii

---
+++ lists.py
@@ -0,0 +1,17 @@
+to_do_list=['study','rest','clean','sufer']
+
+first_item = to_do_list[0]
+second_item = to_do_list[1]
+third_item = to_do_list[2]
+fourth_item = to_do_list[3]
+
+
+print(first_item)
+print(second_item)
+print(third_item)
+print(fourth_item)
+
+shopping_list = ['oranges','bananas','peaches',3,4.5]
+
+shopping_list.append('peaches')
+print(shopping_list)
---
+++ variables.py
@@ -0,0 +1,18 @@
+temperature = 5
+name = "lironi"
+
+print(type(temperature))
+print(type(name))
+
+age=17
+print('my age is', age)
+
+age=age+1
+print('my age now is', age)
+
+first_name = "Liron"
+last_name = "Aliu"
+
+full_name = first_name + ""+ last_name
+
+print(full_name)
