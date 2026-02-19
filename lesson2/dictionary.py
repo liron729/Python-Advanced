ed072dda341ba81d1edea86078316e4b37293e20
liron729 <lironaliu8@gmail.com>
2026-02-19 17:27:51 +0100

    3..

--- lesson2/dictionary.py
+++ lesson2/dictionary.py
@@ -11,8 +11,8 @@ print(contact_info)
 contact_info["egzon"] = "123-456-788"
 print(contact_info)
 
-del contact_info["egzon"]"123-456-788"
-print(contact_info)
+# del contact_info["egzon"] = "123-456-788"
+# print(contact_info)
 
 keys = contact_info.keys()
 print(keys)
@@ -22,3 +22,45 @@ print(values)
 
 items = contact_info.items()
 print(items)
+
+contact_information = {
+    "Liron":{
+        "phone_number" : "043 822 778",
+        "email" : "Lironaliu01@icloud.com",
+        "home_address" : "Fushe Kosove",
+        "birthday" : "01/04/08"
+    },
+
+
+    "Sara" :{
+        "phone_number": "123-123",
+        "email": "sara@icloud.com",
+        "home_address": "Fushe Kosove",
+        "birthday": "24/12/07"
+    },
+
+    "Liron":{
+        "phone_number" : "043 822 778",
+        "email" : "Lironaliu01@icloud.com",
+        "home_address" : "Fushe Kosove",
+        "birthday" : "01/04/08"
+    }
+}
+
+print(contact_information)
+
+sara_information = contact_information["sara"]
+print(sara_information)
+
+contacts = {
+    "Festa":("123-456", "festa@gmail.com"),
+    "sara":("123-123", "sara@gmail.com"),
+    "Liron": ("111-123", "liron@gmail.com")
+}
+
+festa_info = contacts["festa"]
+phone_number = festa_info[0]
+print(phone_number)
+
+
+phone_number, email = contacts["festa"]
---
+++ lesson2/tuples_dictionaries.py
@@ -0,0 +1,9 @@
+grades = {
+    ("Melina", "Math"): 2,
+    ("Dion", "Physics"): 3.5,
+    ("festa", "biology"): 5,
+    ("reina", "English"): 4,
+}
+
+melina_math = grades[("Melina", "math")]
+print("melina's grade in math is", melina_math )
