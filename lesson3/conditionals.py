d045b978cbb19d33230d357687a0b5416cbef23a
liron729 <lironaliu8@gmail.com>
2026-02-24 17:46:29 +0100

    ......................

---
+++ lesson3/conditionals.py
@@ -0,0 +1,25 @@
+from floccinaucinihilipilification import number
+
+age = 18
+
+if age >= 18:
+    print("You can vote.")
+else:
+    print("you cannot vote yet.")
+
+
+temperature = 28
+
+if temperature > 30 :
+    print("its hot day, stay hydrated")
+elif 20 <= temperature <=30 :
+    print("the weather is pleasant")
+else:
+    print("its a cold day")
+
+    number= 7
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print('the number is odd')
