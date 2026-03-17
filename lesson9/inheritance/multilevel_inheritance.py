5868138a7746241ba0ae5b059e00590a53f03a9b
liron729 <lironaliu8@gmail.com>
2026-03-17 17:48:42 +0100

    ....

---
+++ lesson9/inheritance/multilevel_inheritance.py
@@ -0,0 +1,30 @@
+from imghdr import tests
+
+
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model= model
+        self.year = year
+
+    def display_info(self):
+        print(f"Make: {self.make}, Model: {self.model}, Year: {self.year}")
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, body_style):
+        super().__init__(make, model, year)
+        self.body_style = body_style
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year, body_style, Battery_capacity):
+        super().__init__(make, model, year, body_style)
+        self.battery_capacity= battery_capacity
+
+    def charge(self):
+        print("charging the electric car")
+
+tesla = ElectricCar("Tesla", "Cybertruck", 2023, 'triangular', 122.4)
+tesla.display_info()
+print("Body Style:", tesla.body_style)
+print("battery capacity" tesla.battery_capacity)
+tesla.charge()
