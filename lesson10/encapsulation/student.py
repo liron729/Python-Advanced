12ff6e01e5f2313aeb964babbe0b937ab528e3db
liron729 <lironaliu8@gmail.com>
2026-03-24 17:41:15 +0100

    ....

---
+++ lesson10/encapsulation/student.py
@@ -0,0 +1,27 @@
+class Student:
+    def __init__(self, name, age):
+
+        self.__name=name
+        self.__age=age
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name=name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        self.__age=age
+
+student12= Student("dion",17)
+
+print("Name", student12.get_name())
+student12.set_name("Egzon")
+print("Updated Name", student12.get_name)
+
+print("Age", student12.get_age())
+student12.set_age(18)
+print("Update Age:", student12.get_age())
