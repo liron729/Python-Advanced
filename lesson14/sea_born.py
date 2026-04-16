561af272cb60768ca6135f239675fff25881cb9b
liron729 <lironaliu8@gmail.com>
2026-04-16 17:47:04 +0200

    .........

---
+++ lesson14/sea_born.py
@@ -0,0 +1,33 @@
+import sea_born as  sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('avgIQperCountry.csv')
+
+print(df.info())
+
+plt.figure(figsize=(10,6))
+sns.histplot(df['Average IQ'])
+plt.title('Histogram of Average IQ')
+plt.xlabel('Average IQ')
+plt.ylabel('Frequency')
+plt.tight_layout()
+plt.show()
+
+#heatmap
+df['Population - 2023'] = df['Population - 2023'].str.replace(',''').astype(float)
+print(df.info())
+
+numeric_iq_data_loc = df.select_dtypes(include=['number'])
+
+sns.heatmap(numeric_iq_data_loc.corr(), annot=True, cmap="coolwarm", fmt='.2f')
+plt.tight_layout()
+plt.show()
+
+
+plt.figure(figsize=(12,6))
+sns.set_style('darkgrid')
+sns.boxplot(data=df, x='Continent')
+plt.xlabel('Continent')
+plt.tight_layout()
+plt.show()
